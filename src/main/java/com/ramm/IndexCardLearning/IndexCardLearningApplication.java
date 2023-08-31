88060d8d0938f72a8bfa6b72ffc6aee4204ced30
Julian <jramm88@web.de>
2023-08-31 20:38:08 +0200

    Initial commit

---
+++ src/main/java/com/ramm/IndexCardLearning/IndexCardLearningApplication.java
@@ -0,0 +1,13 @@
+package com.ramm.IndexCardLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndexCardLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndexCardLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramm/IndexCardLearning/IndexCardLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramm.IndexCardLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexCardLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
